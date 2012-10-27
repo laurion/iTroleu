1d4ab868f70e738281e638959f96b69042c3a1fa
Teodor Pripoae <teodor.pripoae@gmail.com>
2012-10-27 19:02:21 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ITroleu::Application.config.secret_token = 'b9a7326ad3bd93d6245ffee254588d5bc5b2f863a3e89f705631194a70ba566eb99613d1fdaffe3522840f2deca3148c7a2c8a5c92f810d4ebf055c87058828e'
