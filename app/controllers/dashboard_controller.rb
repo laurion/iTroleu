976ecff0a1a1d595ac0f507913e95ddab311050e
Teodor Pripoae <teodor.pripoae@gmail.com>
2012-10-28 01:30:07 +0300

    Dev commit

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ class DashboardController < ApplicationController
   def start
   end
 
-  def end
+  def destination
+    session[:start_coords] = {:long => params[:long], :lat => params[:lat]}
   end
 
   def search
